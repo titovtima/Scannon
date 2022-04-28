309cfae0f67612b524e4d0086caee7e61ad5accb
titovtima <titov.tima.spb@yandex.ru>
2022-04-28 12:32:53 +0300

    fix incorrect scaling

--- js/Utilities/Scaler.js
+++ js/Utilities/Scaler.js
@@ -2,7 +2,7 @@ class Scaler {
 
     static setResolution(scene, width, height) {
         scene.sys.game.scale.setGameSize(width, height);
-        scene.scale.parentSize.setSize(width, height);
+        // scene.scale.parentSize.setSize(width, height);
         scene.scale.displaySize.setAspectRatio(width / height);
         scene.physics.world.setBounds(0, 0, width, height);
     }
